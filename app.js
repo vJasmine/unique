e6de962d1df4455486a5ed86a23a00a601dc004a
vJasmine <41657091+vJasmine@users.noreply.github.com>
2018-11-23 18:05:26 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// 引入express
 const express=require('express');
-const pool=require('./pool.js');
-// 构建web服务器
+const bodyParser=require('body-parser');
+// 引入路由模块
+var register=require('./routes/register.js');
+var index=require('./routes/index.js');
+// 创建express模块
 var app=express();
+// 绑定监听端口
 app.listen(3000);
-// 托管静态资源
-app.use(express.static('./public'));
+// 绑定静态目录
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+// 使用路由器管理路由
+app.use("/register",register);
+app.use("/index",index);
